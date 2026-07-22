3a064f467b7f4dcd569eb920cffac029ae5c06c3
Roman Ianchuk <yan4uk1001@gmail.com>
2026-07-22 22:00:33 +0100

    Create 01_clean_sales_data.sql with header comments
    Add initial comments for sales data cleaning script

---
+++ sql/01_clean_sales_data.sql
@@ -0,0 +1,5 @@
+-- Sales Analysis Dashboard
+-- BigQuery project: sales-dashboard-project-460219
+-- IMPORTANT: In BigQuery Query settings, set Data location to europe-west4
+-- before running this script.
+
